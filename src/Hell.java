e57323a97d5e1e5d6ce594cfe72e2e79d2db043f
Kim sungsoo <tjdtnsla0911@naver.com>
2020-05-27 18:56:49 +0800

    안녕!

---
+++ src/Hell.java
@@ -0,0 +1,6 @@
+
+public class Hell {
+public static void main(String[] args) {
+	System.out.println("What the Hell");
+}
+}
---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		Random r= new Random();
+		System.out.println(r.nextInt(15)+1);
+
+	}
+
+}
